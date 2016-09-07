9d43204afac817afb757a3f5bece5b6431e5c0cc
puzzud <puzzud@gmail.com>
2016-09-06 21:28:08 -0400

    Added unimplemented SetCharacterColor to C64 video.c, to allow for compilation.

--- src/c64/video.c
+++ src/c64/video.c
@@ -4,12 +4,19 @@
 
 void __fastcall__ InitVideo(void)
 {
-  SetBackgroundColor(0);
+  
 }
 
 void __fastcall__ SetBackgroundColor(unsigned char color)
 {
   SET_MEMORY(53281l, color)
+}
+
+void __fastcall__ SetCharacterColor(unsigned char index, unsigned char color)
+{
+  // TODO: Determine best way to handle this function,
+  // as C64 is not limited to character graphics being
+  // of 4 different palette entries.
 }
 
 void __fastcall__ PrintText(const unsigned char* text, unsigned char x, unsigned char y)
